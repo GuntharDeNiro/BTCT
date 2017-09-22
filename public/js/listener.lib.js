653dcd37c5e1406ee1e999a6c394037446e81aa4
3kynox <thierry.prost@gmail.com>
2017-09-22 23:47:28 +0200

    Trade History now populated

--- public/js/listener.lib.js
+++ public/js/listener.lib.js
@@ -1,5 +1,6 @@
 let connected = false;
 
+/* Manage cases currencies having 3 letters or more */
 function getBaseCurrency(pair) {
     if (pair.startsWith('DASH')) {
         return pair.substr(4, 3);
@@ -20,8 +21,8 @@ function getQuoteCurrency(pair) {
     }
 }
 
+/* Split URL to get current selected exchange and currency */
 var urlParts = window.location.href.split("/").filter(function(n){ return n !== "" });
-
 var exchange = urlParts[urlParts.length - 2];
 var pair = urlParts[urlParts.length - 1];
 var pairParts = [];
@@ -38,8 +39,7 @@ if(exchange === "kraken") {
     pairParts = [getBaseCurrency(pair), getQuoteCurrency(pair)];
 }
 
-
-
+/* Manage incoming ws messages and sort it to the view (listener.html) */
 function processMessage(event) {
 
     var input = JSON.parse(event.data);
@@ -59,18 +59,13 @@ function processMessage(event) {
 
     switch (input.event) {
         case 'getting_balances':
-
-            console.log(pairParts);
-
             var dataElement1 = input.data[pairParts[0]];
             dataElement1.header = pairParts[0];
 
             var dataElement2 = input.data[pairParts[1]];
             dataElement2.header = pairParts[1];
 
-
             var formattedData = [dataElement1, dataElement2];
-            console.log(formattedData);
 
             $('table.getting_balances.' + input.exchange + "." + input.pair).dynatable({
                 features: {
@@ -86,18 +81,24 @@ function processMessage(event) {
                 }
 
             });
+
             break;
 
         case 'gettting_trade_history':
-        case 'getting_open_orders':
-            console.log(input.pair);
-
-
             $('table.' + input.event + '.' + input.exchange + "." + input.pair).dynatable({
                 dataset: {
                     records: input.data
                 }
             });
+            break;
+
+        case 'getting_open_orders':
+            $('table.' + input.event + '.' + input.exchange + "." + input.pair).dynatable({
+                dataset: {
+                    records: input.data
+                }
+            });
+
             break;
 
         case 'getting_order_book':
@@ -113,29 +114,26 @@ function processMessage(event) {
                     $("." + input.exchange + "." + input.pair + "." + input.event + "." + k).text(input.data[k]);
                 }
             }
+
             break;
+
         case 'message':
             var message = input.data;
             $("." + input.exchange + "." + input.pair + ".messages_list").prepend("<li>" + message + "</li>");
-
     }
-
 }
 
+/* Loop to receive incoming data from gunbot */
 setInterval(()=>{
-
-    // console.log("tick");
-
     if (connected)
         return false;
 
-    var exampleSocket = new WebSocket("wss://"+config.hostname+":"+config.port+"/?gui=native");
+    var socket = new WebSocket("wss://"+config.hostname+":"+config.port+"/?gui=native");
     console.log("connected");
     connected = true;
 
-    exampleSocket.onmessage = processMessage;
-
-    exampleSocket.onerror = function (event) {
+    socket.onmessage = processMessage;
+    socket.onerror = function (event) {
         connected = false;
         console.log(event);
     };
@@ -147,9 +145,5 @@ setInterval(()=>{
     //         records: [{"orderNumber":"120466","type":"sell","rate":"0.025","amount":"100","total":"2.5"},{"orderNumber":"120467","type":"sell","rate":"0.04","amount":"100","total":"4"} ]
     //     }
     // });
-
-
-
-
 
 }, 1000);
